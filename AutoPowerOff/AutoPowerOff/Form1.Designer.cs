4c43c6bae9b0c34b3a7a03839d295616c3bee769
mfright <uj020288@yahoo.co.jp>
2019-09-20 20:26:52 +0900

    ver1.0

---
+++ AutoPowerOff/AutoPowerOff/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace AutoPowerOff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblCountDown = new System.Windows.Forms.Label();
+            this.myTimer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.lblInfo.Location = new System.Drawing.Point(12, 9);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(250, 27);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "00分後にシャットダウン";
+            // 
+            // lblCountDown
+            // 
+            this.lblCountDown.AutoSize = true;
+            this.lblCountDown.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.lblCountDown.Location = new System.Drawing.Point(12, 57);
+            this.lblCountDown.Name = "lblCountDown";
+            this.lblCountDown.Size = new System.Drawing.Size(105, 40);
+            this.lblCountDown.TabIndex = 1;
+            this.lblCountDown.Text = "00:00";
+            // 
+            // myTimer1
+            // 
+            this.myTimer1.Enabled = true;
+            this.myTimer1.Interval = 1000;
+            this.myTimer1.Tick += new System.EventHandler(this.myTimer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 127);
+            this.Controls.Add(this.lblCountDown);
+            this.Controls.Add(this.lblInfo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AutoPowerOff";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblCountDown;
+        private System.Windows.Forms.Timer myTimer1;
+    }
+}
+
---
+++ AutoPowerOff/AutoPowerOff/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace AutoPowerOff
+{
+    public partial class Form1 : Form
+    {
+        int minutes = 0;
+        int seconds = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadIni();
+        }
+
+        private void loadIni()
+        {
+            StreamReader sr = new StreamReader(@"minutes.ini", Encoding.GetEncoding("Shift_JIS"));
+
+            string str = sr.ReadToEnd();
+
+            minutes = int.Parse(str);
+
+            sr.Close();
+
+            lblInfo.Text = minutes + "分後にシャットダウン";
+
+            lblCountDown.Text = minutes + ":" + seconds;
+        }
+
+        private void myTimer1_Tick(object sender, EventArgs e)
+        {
+            if(seconds != 0)
+            {
+                seconds--;
+            }
+            else if(minutes != 0)
+            {
+                minutes--;
+                seconds = 59;
+            }
+            else
+            {
+                execShutdown();
+                myTimer1.Stop();
+            }
+
+            lblCountDown.Text = minutes + ":" + seconds;
+        }
+
+        private void execShutdown()
+        {
+            System.Diagnostics.Process pro = new System.Diagnostics.Process();
+
+            pro.StartInfo.FileName = "shutdown";            // コマンド名
+            pro.StartInfo.Arguments = "-s -t 1";               // 引数
+            pro.StartInfo.CreateNoWindow = true;            // DOSプロンプトの黒い画面を非表示
+            pro.StartInfo.UseShellExecute = false;          // プロセスを新しいウィンドウで起動するか否か
+            pro.StartInfo.RedirectStandardOutput = true;    // 標準出力をリダイレクトして取得したい
+
+            pro.Start();
+
+            string output = pro.StandardOutput.ReadToEnd();
+        }
+    }
+}
